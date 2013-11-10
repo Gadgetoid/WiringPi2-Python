886740d584ed6b96a86676bdc2c4980c80435787
robotdad <mrgoodner@hotmail.com>
2013-11-10 03:34:14 +0000

    Changes for local compile on Pi (Raspbian)

--- WiringPi/wiringPi/max31855.c
+++ WiringPi/wiringPi/max31855.c
@@ -22,8 +22,8 @@
  ***********************************************************************
  */
 
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
+#include "wiringPi.h"
+#include "wiringPiSPI.h"
 
 #include "max31855.h"
 
